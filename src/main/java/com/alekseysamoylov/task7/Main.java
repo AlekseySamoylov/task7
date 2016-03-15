b60c3a6ceb6c8e8e4d582077cc44ad7659fb0c41
Алексей Самойлов <AlekseiSamoilov@Aleksejs-iMac.local>
2016-03-15 18:37:45 +0500

    all

---
+++ src/main/java/com/alekseysamoylov/task7/Main.java
@@ -0,0 +1,7 @@
+package com.alekseysamoylov.task7;
+
+/**
+ * Created by Aleksey Samoylov on 29.12.2015.
+ */
+public class Main {
+}
---
+++ src/main/java/com/alekseysamoylov/task7/controllers/FileUploaderController.java
@@ -0,0 +1,13 @@
+package com.alekseysamoylov.task7.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Aleksey Samoylov on 29.12.2015.
+ */
+@Controller
+public class FileUploaderController {
+    @RequestMapping(value="/singleUpload")
+
+}
